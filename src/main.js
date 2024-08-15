0495c47e17e38b1dc48aa98e7257dee03fa969ac
Nassim <jslegend@protonmail.com>
2024-08-14 22:48:19 -0400

    refactored logic for platforms infinite scrolling

--- src/main.js
+++ src/main.js
@@ -28,21 +28,8 @@ k.scene("game", () => {
   ];
 
   const platforms = [
-    k.add([
-      k.sprite("platforms", { tiled: true, width: 384, height: 160 }),
-      k.pos(0, 450),
-      k.scale(4),
-    ]),
-    k.add([
-      k.sprite("platforms", { tiled: true, width: 384, height: 160 }),
-      k.pos(384, 450),
-      k.scale(4),
-    ]),
-    k.add([
-      k.sprite("platforms", { tiled: true, width: 384, height: 160 }),
-      k.pos(384 * 2, 450),
-      k.scale(4),
-    ]),
+    k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(4)]),
+    k.add([k.sprite("platforms"), k.pos(384, 450), k.scale(4)]),
   ];
 
   const sonic = k.add([
@@ -93,16 +80,13 @@ k.scene("game", () => {
     bgPieces[0].moveTo(bgPieces[0].pos.x, -sonic.pos.y / 10 - 50);
     bgPieces[1].moveTo(bgPieces[1].pos.x, -sonic.pos.y / 10 - 50);
 
-    for (const platform of platforms) {
-      if (platform.pos.x < -1920) {
-        platforms.push(platforms.shift());
-        platform.moveTo(1920, 450);
-      }
+    if (platforms[1].pos.x < 0) {
+      platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
+      platforms.push(platforms.shift());
     }
 
     platforms[0].move(-4000, 0);
-    platforms[1].moveTo(platforms[0].pos.x + 1532, 450);
-    platforms[2].moveTo(platforms[1].pos.x + 1532, 450);
+    platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
   });
 });
 
